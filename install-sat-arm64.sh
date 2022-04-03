28e4e11c30e9cc65cbb7bd38c75b0982468e6dea
Philippe Charrière <ph.charriere@gmail.com>
2022-04-03 13:14:46 +0000

    📝 save

--- install-sat-arm64.sh
+++ install-sat-arm64.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+RELEASE="0.1.1"
+ARCHITECTURE="arm64"
 # ------------------------------------
 # Install sat
 # ------------------------------------
-
+cp sat-arm64/sat-${RELEASE}-${ARCHITECTURE} procyon/executors/sat
--- sat-arm64/build-sat-arm64.sh
+++ sat-arm64/build-sat-arm64.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 RELEASE="0.1.1"
 ARCHITECTURE="arm64"
-# ------------------------------------
-# WasmEdge dependencies
-# ------------------------------------
-curl -sSf https://raw.githubusercontent.com/WasmEdge/WasmEdge/master/utils/install.sh | bash
-
-source $HOME/.wasmedge/env
 
 # ------------------------------------
 # Install sat
@@ -14,7 +8,7 @@ source $HOME/.wasmedge/env
 git clone --depth=1 https://github.com/suborbital/sat.git
 
 cd sat
-go build -o .bin/sat -tags netgo,wasmedge .
-cp sat ../sat-${RELEASE}-${ARCHITECTURE}
+go build -o .bin/sat -tags netgo,wasmtime .
+cp .bin/sat ../sat-${RELEASE}-${ARCHITECTURE}
 cd ..
 rm -rf sat
